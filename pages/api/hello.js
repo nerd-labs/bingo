25bafc03e5c57be28258bb409cac6fb21f8793eb
Robbert Wolfs <robbertwolfs@gmail.com>
2021-05-17 14:56:52 +0200

    fix(bingo): revert

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-//
-const publicIp = require('public-ip');
 
-
-export default async (req, res) => {
-    console.log(await publicIp.v4());
-
-    res.status(200).json({ name: 'John Doe' })
+export default (req, res) => {
+  res.status(200).json({ name: 'John Doe' })
 }
