e2992e273b74d1dbe3d4d042018a4cff7695a4ec
Robbert Wolfs <robbertwolfs@gmail.com>
2021-05-17 14:53:10 +0200

    fix(bingo): add bingo button

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//
+const publicIp = require('public-ip');
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async (req, res) => {
+    console.log(await publicIp.v4());
+
+    res.status(200).json({ name: 'John Doe' })
 }
