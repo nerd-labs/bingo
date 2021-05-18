be5c389bef06313b12b4bc886e670a608c545139
Robbert Wolfs <robbertwolfs@gmail.com>
2021-05-18 12:21:11 +0200

    fix(bingo): add data

--- src/config.js
+++ src/config.js
@@ -1,71 +1,248 @@
-const superJackpot = [
-  {
-    name: 'Scooter',
-    big: true
-  },
-  {
-    name: 'Nintendo switch lite',
-    big: true
-  },
-  {
-    name: 'Bitcoin',
-    big: true
-  },
-  {
-    name: 'Jacuzzi',
-    big: true
-  },
-  {
-    name: 'Pokemon cards',
-    big: true
-  },
-  {
-    name: 'GoPro'
-  },
-  {
-    name: 'Mouse'
-  },
-  {
-    name: 'Airpods'
-  },
-  {
-    name: 'Phone'
-  },
-  {
-    name: 'iPad'
-  },
-  {
-    name: 'Arduino'
-  },
-  {
-    name: 'Netflix'
-  },
-  {
-    name: 'Disney+'
-  },
-  {
-    name: 'Board game'
-  },
-  {
-    name: 'Pencils'
-  },
-  {
-    name: 'Holiday'
-  },
-  {
-    name: 'Lego'
-  },
-  {
-    name: 'Wine'
-  },
-  {
-    name: 'BBQ'
-  },
-  {
-    name: 'Gin'
-  }
-];
+const level1 = {
+    prizes: [
+        {
+            name: '100 mondmaskers',
+            url: 'https://www.bol.com/nl/p/3laags-wegwerp-mondkapjes-50-stuks-niet-medisch/9300000002383984/',
+        },
+        {
+            name: 'Lego gift cards',
+            url: 'https://www.lego.com/en-us/page/static/gift-cards/give',
+        },
+        {
+            name: 'Cinematickets Kinepolis',
+            url: 'https://shop.kinepolis.be/nl',
+        },
+        {
+            name: 'Kubb set',
+            url: 'https://www.bol.com/nl/p/outdoor-play-kubb-game/9200000022663906',
+        },
+        {
+            name: 'Badmintonset',
+            url: 'https://www.decathlon.be/nl/p/badmintonnet-easy-set-3-m/_/R-p-300055',
+        },
+        { 
+            name: 'Catan - Bordspel',
+            url: 'https://www.bol.com/nl/p/catan-basisspel/1004004012362052',
+        },
+        {
+            name: 'Dominion - Kaartspel',
+            url: 'https://www.bol.com/nl/p/dominion-basisspel-kaartspel/9200000080798542',
+        },
+        {
+            name: 'Broodbakmachine',
+            url: 'https://www.bol.com/nl/p/inventum-bm55-broodbakmachine/9200000025955257',
+        },
+        {
+            name: 'Durbuy Adventure Day (1p)',
+            url: 'https://www.adventure-valley.be/nl/pass/adventure-pass',
+        },
+        {
+            name: 'Ticket Bobbejaanland (1p)',
+            url: 'https://www.bobbejaanland.be/plan-je-bezoek/tickets',
+        },
+        {
+            name: 'Trui met boodschap',
+            url: 'https://antwerponly.com/collections',
+        },
+        {
+            name: 'Logitech Wireless Mouse',
+            url: 'https://www.logitech.com/en-us/products/mice/mx-master-3-mac-wireless-mouse.910-005693.html',
+            big: true,
+        },
+        {
+            name: 'Bongobon: Glamping',
+            url: 'https://www.bongo.be/nl/cadeaubonnen/weekendje-weg-overnachten/glamping-884583.html',
+            big: true,
+        },
+        { 
+            name: 'JBL Bluetooth speakers',
+            url: 'https://www.coolblue.be/nl/product/818429/jbl-charge-4-zwart.html',
+            big: true,
+        },
+        {
+            name: 'Good Beer Spa - Gift card',
+            url: 'https://goodbeerspa.com/product-category/giftcards/',
+            big: true,
+        },
+        {
+            name: 'Alpacawandeling',
+            url: 'https://www.alpacaboerderij.be/activiteit/4/Korte-Alpacawandeling',
+            big: true,
+        },
+    ],
+    extraQuestion: 'Maak een het langst mogelijke woord met letterkoekjes.',
+};
+
+const level2 = {
+    prizes: [
+        {
+            name: 'Ticket Plopsa-land',
+            url: 'https://www.plopsalanddepanne.be/nl/tickets',
+        },
+        {
+            name: 'Pandemic - Bordspel',
+            url: 'https://www.bol.com/nl/p/pandemic-engelstalig-bordspel/9200000018019326/',
+        },
+        {
+            name: 'Ticket Efteling',
+            url: 'https://www.efteling.com/nl/park/informatie/toegangsprijzen',
+        },
+        {
+            name: 'Vlogset',
+            url: 'https://www.bol.com/nl/p/ringlamp-vlogset-compleet-verstelbaar-statief-tafel-statief-incl-telefoonhouder-8-inch-usb-tiktok-ringlight-flitser-ring-lamp-vlog-make-up-light-studiolamp-vloggen-tutorial/9300000004155333',
+        },
+        {
+            name: 'Kanjam',
+            url: 'https://www.bol.com/nl/p/kanjam-game-set/9200000022847660',
+        },
+        { 
+            name: 'Steam - Gift card',
+            url: 'https://store.steampowered.com/digitalgiftcards/',
+        },
+        {
+            name: 'Voetbal prentjes Panini',
+            url: 'https://www.bol.com/nl/p/panini-uefa-euro-2020-tournament-edition-50-zakjes-doosje/9300000035508848',
+        },
+        {
+            name: 'Netflix - Gift card ',
+            url: 'https://startselect.com/be-nl/netflix',
+        },
+        {
+            name: 'Bongobon: Verwen-moment voor twee',
+            url: 'https://www.bongo.be/nl/cadeaubonnen/spa-en-wellness/verwenmoment-voor-twee-1052297.html',
+        },
+        {
+            name: 'Darts',
+            url: 'https://www.dartshopper.nl/winmau-blade-5-dartbord-2-sets-steeldarts.html',
+        },
+        {
+            name: 'Wijnkoel-zak',
+            url: 'https://www.cubicoolstore.com/en/',
+        },
+        {
+            name: 'Soda-stream',
+            url: 'https://www.bol.com/nl/p/sodastream-spirit-megapack-black-incl-3-flessen/9200000077986346/',
+        },
+        {
+            name: 'Spikeball',
+            url: 'https://www.bol.com/nl/p/spikeball-metaball-outdoor-indoor-spike-ball-speelset-spikeball-amerikaanse-sport-hit-bal-spel-outdoor-strandspel/9300000024968812',
+        },
+        { 
+            name: 'Festival tent',
+            url: 'https://www.decathlon.be/nl/p/buitentent-voor-quechua-tent-2-seconds-3-xl-fresh-black/_/R-p-304345',
+        },
+        {
+            name: 'Mini-keuken voor de kindjes',
+            url: 'https://www.ikea.com/be/nl/p/duktig-keukentje-berken-60319972',
+        },
+        {
+            name: 'Airfryer',
+            url: 'https://www.coolblue.be/nl/product/882488/philips-airfryer-xl-hd9263-90.html',
+            big: true,
+        },
+        {
+            name: 'Bongobon: Ballon-vaart',
+            url: 'https://www.bongo.be/nl/cadeaubonnen/actie-en-avontuur/ballonvaart-bubbels-884196.html',
+            big: true,
+        },
+        {
+            name: 'Mini ballenbad',
+            url: 'https://www.degeleflamingo.com/collections/ballenbadjes/products/misioo-ballenbad-xxl-vierkant-lichtgrijs-pre-order-levering-22-11',
+            big: true,
+        },
+        {
+            name: 'Steelstofzuiger',
+            url: 'https://www.coolblue.be/nl/product/792925/aeg-cx7-2-35o.html',
+            big: true,
+        },
+        {
+            name: 'Airpods Pro',
+            url: 'https://www.bol.com/nl/p/apple-airpods-pro/9200000123229147/',
+            big: true,
+        },
+        {
+            name: 'The Jane (2p) - Waarde-bon',
+            url: 'https://www.thejaneantwerp.com/eng/shop',
+            big: true,
+        }
+    ],
+    extraQuestion: 'Maak de langste worst met een potje Play-Doh.',
+};
+
+const superJackpot = {
+    prizes: [
+        {
+            name: 'Scooter',
+            big: true,
+            url: '',
+        },
+        {
+            name: 'Nintendo switch lite',
+            big: true
+        },
+        {
+            name: 'Bitcoin',
+            big: true
+        },
+        {
+            name: 'Jacuzzi',
+            big: true
+        },
+        {
+            name: 'Pokemon cards',
+            big: true
+        },
+        {
+            name: 'GoPro'
+        },
+        {
+            name: 'Mouse'
+        },
+        {
+            name: 'Airpods'
+        },
+        {
+            name: 'Phone'
+        },
+        {
+            name: 'iPad'
+        },
+        {
+            name: 'Arduino'
+        },
+        {
+            name: 'Netflix'
+        },
+        {
+            name: 'Disney+'
+        },
+        {
+            name: 'Board game'
+        },
+        {
+            name: 'Pencils'
+        },
+        {
+            name: 'Holiday'
+        },
+        {
+            name: 'Lego'
+        },
+        {
+            name: 'Wine'
+        },
+        {
+            name: 'BBQ'
+        },
+        {
+            name: 'Gin'
+        }
+    ],
+    extraQuestion: '',
+};
 
 export default {
-  superJackpot
+    level1,
+    level2,
+    superJackpot
 };
