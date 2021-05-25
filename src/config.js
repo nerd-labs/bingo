c0cd9cba27cc54a818d1df1113408151a2bf9e5d
Robbert Wolfs <robbertwolfs@gmail.com>
2021-05-25 20:57:27 +0200

    fix(bingo): update prize

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const level1 = {
             url: 'https://www.bol.com/nl/p/lego-creator-safari-wilde-dieren-boomhuis-31116/9300000019666264',
         },
         {
-            name: 'Ticket Bobbejaanland',
+            name: 'Ticket Bobbejaanland (1p)',
             url: 'https://www.bobbejaanland.be/plan-je-bezoek/tickets',
         },
         {
